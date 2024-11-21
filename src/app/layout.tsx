383ae4b5d5f330e018352c6b90927ced1e969538
Fred Rukundo <dukefred9@gmail.com>
2024-11-21 15:04:41 +0100

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-black text-white">
         {children}
       </body>
     </html>
