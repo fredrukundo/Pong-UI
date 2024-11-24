025c975e0a3396d878cb695b0435300a06fd57e7
Fred Rukundo <dukefred9@gmail.com>
2024-11-24 03:04:25 +0100

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Game from "@/components/Game";
 
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-[#d1fae5]">
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-[#d1fae5]">
       <Game/>
     </div>
   );
