445fcd52b34c5fc1080ba5346f060548e3ae8287
Fred Rukundo <dukefred9@gmail.com>
2024-11-29 22:40:44 +0100

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Game from "@/components/Game";
-
-
+import Link from "next/link";
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-[#d1fae5]">
-      <Game/>
+   
+    <div className="min-h-screen bg-[#bae6fd] flex flex-col items-center justify-center ">
+      <header className="text-center mb-8">
+        <h1 className="text-5xl font-bold">Welcome to Transcendance </h1>
+        <p className="text-lg my-4 font-sans">Explore The available Games</p>
+        <Link href="/game">
+          <button className="bg-blue-700 text-white px-6 py-3 rounded-full font-semibold"> Play Games</button>
+          </Link>
+      </header>
     </div>
   );
 }
